75df1a14dd9efe5ac7e59b90f0e812f71d8ad449
Caique <caique_sarkis_corebiz@whirlpool.com>
2022-02-09 01:52:50 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import database from './models'
+
+
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send(database)
+})
+
+
+const PORT = 3000
+
+app.listen(PORT);
+
+console.log(`Server started at http://localhost:${PORT}`)
+
---
+++ src/models/index.js
@@ -0,0 +1,18 @@
+const database = {
+	questions:[
+		{
+			question: "blabaa la skdfj...?",
+			options: ["option1","option2","..."],
+			answer: "option5"
+		},
+		{
+			question: "sdfdsfdsf ssss sdfsdfsdfs...?",
+			options: ["option1","option2","..."],
+			answer: "option1"
+		}
+	]
+}
+
+export default {
+  database,
+};
